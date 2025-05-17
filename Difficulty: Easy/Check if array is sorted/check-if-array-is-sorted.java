0ce25974c3f4d28b7a709827d3852e2d501257d6
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-05-17 10:45:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    public boolean arraySortedOrNot(int[] arr) {
+        // code here
+            int count =0;
+            for(int i=0;i<arr.length-1;i++){
+                if(arr[i]<=arr[i+1]){
+                    count++;
+                }
+                
+            }
+            if(count==arr.length-1){
+                return true;
+            }
+            else{
+                return false;
+            }
+           
+       }
+    }
+
+
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        while (t-- > 0) {
+            String line = scanner.nextLine();
+            String[] numStrings = line.split(" ");
+            int arr[] = new int[numStrings.length];
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = Integer.parseInt(numStrings[i]);
+            Solution ob = new Solution();
+            boolean ans = ob.arraySortedOrNot(arr);
+            System.out.println(ans ? "true" : "false");
+            System.out.println("~");
+        }
+        scanner.close();
+    }
+}
+// } Driver Code Ends
