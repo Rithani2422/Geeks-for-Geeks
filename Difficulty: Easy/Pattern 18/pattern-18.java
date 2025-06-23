c9a97739eef7c58e97d92a8cd9f5d0558c15630b
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-06-23 18:49:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 18/pattern-18.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+           
+        for(int i=0;i<n;i++){
+             int start=65;
+            for(int j=0;j<=i;j++){
+                System.out.print((char)(start+n-1)+" ");
+                start--;
+                
+            }
+            System.out.println();
+        }
+    }
+}
