37891c41b816bbff89895bfde03511c3ea8d8e79
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-06-24 13:02:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 21/pattern-21.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(N):
+            for j in range(N):
+                if i==0 or i==N-1 or j==0 or j==N-1:
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+            print()
+            
