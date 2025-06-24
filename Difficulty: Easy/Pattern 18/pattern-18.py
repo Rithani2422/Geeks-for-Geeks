25ec07e206ef35f294c34cf0b2d9b4d0c52549d9
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-06-24 12:24:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 18/pattern-18.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(1,N+1):
+            start=65
+            for j in range(i):
+                print(chr(start+N-1),end=" ")
+                start-=1
+            print()
