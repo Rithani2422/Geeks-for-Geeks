cb77fb390ced4bfa54b89e3021d86e980f827495
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-07-28 11:03:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,60 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] arr = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) arr[idx++] = i;
-
-            new Solution().mergeSort(arr, 0, arr.length - 1);
-
-            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
-
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
 
     void mergeSort(int arr[], int l, int r) {
         // code here
-        if(l==r){
-            return;
-        }
+        if(l>=r)return;
         int mid=(l+r)/2;
         mergeSort(arr,l,mid);
         mergeSort(arr,mid+1,r);
-        combine(arr,l,mid,r);
-        
+        merge(arr,l,mid,r);
     }
-    void combine(int[] arr,int l,int mid,int high){
-        ArrayList<Integer> list=new ArrayList<>();
+    void merge(int arr[],int l,int mid, int r){
+        List<Integer> list=new ArrayList<>();
         int left=l,right=mid+1;
-        while(left<=mid && right<=high){
-            if(arr[left]<arr[right]){
+        while(left<=mid && right<=r){
+            if(arr[left]<=arr[right]){
                 list.add(arr[left]);
                 left++;
             }
@@ -63,17 +21,16 @@ class Solution {
                 right++;
             }
         }
-            while(left<=mid){
-                list.add(arr[left]);
-                left++;
-            }
-            while(right<=high){
-                list.add(arr[right]);
-                right++;
-            }
-            for(int i=l;i<=high;i++){
-                arr[i]=list.get(i-l);
-            }
+        while(left<=mid){
+            list.add(arr[left]);
+            left++;
+        }
+        while(right<=r){
+            list.add(arr[right]);
+            right++;
+        }
+        for(int i=l;i<=r;i++){
+            arr[i]=list.get(i-l);
         }
     }
-
+}
