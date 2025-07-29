6b20d9df84eea1596112ac566d0ed53c1ac71710
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-07-29 20:18:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.py
+++ Difficulty: Basic/Reverse a String/reverse-a-string.py
@@ -3,13 +3,13 @@
 class Solution:
      def reverseString(self, s: str) -> str:
         # code here
-        arr=list(s)
-        start=0
-        end=len(arr)-1
-        while(start<end):
-            temp=arr[start]
-            arr[start]=arr[end]
-            arr[end]=temp
-            start+=1
-            end-=1
-        return ''.join(arr)
+        s=list(s)
+        i=0
+        j=len(s)-1
+        while i<j:
+            s[i],s[j]=s[j],s[i]
+           
+            i+=1
+            j-=1
+        return ''.join(s)
+     
